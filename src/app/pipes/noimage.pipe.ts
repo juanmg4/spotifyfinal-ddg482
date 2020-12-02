7e7cc9b80d5e6ffe44840e198930a2010b501f14
juanmg4 <existetodoche2@gmail.com>
2020-12-01 22:20:34 -0300

    5 files modified

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -1,19 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'noimage',
+  name: "noimage"
 })
 export class NoimagePipe implements PipeTransform {
-
   transform(images: any[]): string {
     /* Compruebo si el arreglo de imagenes existe o no. Si no existe devuelvo la imagen de que no hay imagen */
-    if (!images){
-      return 'assets/img/noimage.png';
-    /* si existe el arreglo y hay imagenes, devuelvo la que esta en la posicion 0. Si el arrreglo esta vacio y no hay imagenes, devuelvo la imagen de que no hay imagen */
-    }else if (images.length > 0) {
+    if (!images) {
+      return "assets/img/noimage.png";
+      /* si existe el arreglo y hay imagenes, devuelvo la que esta en la posicion 0. Si el arrreglo esta vacio y no hay imagenes, devuelvo la imagen de que no hay imagen */
+    } else if (images.length > 0) {
       return images[0].url;
     } else {
-      return 'assets/img/noimage.png';
+      return "https://i.ibb.co/9hjxPn2/noimage.jpg";
     }
   }
 }
